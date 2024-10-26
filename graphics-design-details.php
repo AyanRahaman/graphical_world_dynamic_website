6464da9c3d67b57f15480d675f29951d3ed7d40d
Ayan Rahaman <ayanrahaman041@gmail.com>
2024-10-26 13:23:31 +0530

    commit

--- graphics-design-details.php
+++ graphics-design-details.php
@@ -33,19 +33,20 @@ require_once("common_content/navbar.php");
  <!-- ===== BLOG PART START ===== -->
  <div class="container mb-5">
     <div class="row g-5 mt-3">
-      <div class="col-md-8">
-        <img src="image/graphic_design_service.jpg" height="200px" width="100%" class="" alt="..." />
-        <article class="blog-post shadow p-3 bg-white">
-        <?php
+                                      <?php
                                        require_once("dashboard/partials/DBconnect.php");
                                        
                                        $sql = "SELECT * FROM s_content WHERE id = 1";
                                        $stmt = $connectingDB->query($sql);
                                        while ($Data = $stmt->fetch()) {
+                                           $image = $Data["image"];
                                            $about = $Data["about"];
                                            $type = $Data["type"];
                                        }
                                        ?>
+      <div class="col-md-8">
+        <img src="dashboard/images/<?php echo $image; ?>" height="200px" width="100%" class="" alt="<?php echo $image; ?>" />
+        <article class="blog-post shadow p-3 bg-white">
           <h2 class="display-5 link-body-emphasis mb-3 fw-bold text-center">
             <?php echo $type; ?>
           </h2>
@@ -60,32 +61,16 @@ require_once("common_content/navbar.php");
           <div class="our_services">
             <div class="row">
               <div class="col-lg-4">
+                                    <?php
+                                       $sql2 = "SELECT * FROM s_services WHERE type = 'Graphics Design'";
+                                       $stmt2 = $connectingDB->query($sql2);
+                                       while ($Data2 = $stmt2->fetch()) {
+                                           $s_s_name = $Data2["s_s_name"];
+                                       
+                                       ?>
                 <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Logo Design</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">LetterHeads</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Flyers</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Brochur</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Info graphic</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Stickers</span><br />
-              </div>
-              <div class="col-lg-4">
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Banners</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Visiting Card</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Business Card</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Social Media Post</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Creative Add</span><br />
-                <i class="fa-solid fa-check-double" style="color: #3440f4"></i>
-                <span class="fw-bold">Branding and More....</span><br />
+                <span class="fw-bold"><?php echo $s_s_name; ?></span><br />
+                <?php } ?>
               </div>
             </div>
           </div>
@@ -94,21 +79,17 @@ require_once("common_content/navbar.php");
             <h5>Some common projects we have wroked on included</h5>
           </div>
           <ul class="list-unstyled">
+                                  <?php
+                                       $sql3 = "SELECT * FROM s_s_c_project WHERE type = 'Graphics Design'";
+                                       $stmt3 = $connectingDB->query($sql3);
+                                       while ($Data3 = $stmt3->fetch()) {
+                                           $ssc_name = $Data3["ssc_name"];
+                                       
+                                       ?>
             <li>
-              <i class="fa-solid fa-diamond text-warning"></i> Brochures, rack cards & booklets
+              <i class="fa-solid fa-diamond text-warning"></i> <?php echo $ssc_name; ?>
             </li>
-            <li>
-              <i class="fa-solid fa-diamond text-warning"></i> Logos and branded elements
-            </li>
-            <li>
-              <i class="fa-solid fa-diamond text-warning"></i> Posters, banners and signage
-            </li>
-            <li>
-              <i class="fa-solid fa-diamond text-warning"></i> Social media graphics & digital marketing ads
-            </li>
-            <li>
-              <i class="fa-solid fa-diamond text-warning"></i> Email marketing graphics
-            </li> 
+            <?php } ?>
           </ul>
           <hr>
 
@@ -116,7 +97,7 @@ require_once("common_content/navbar.php");
                     
 
           <nav class="blog-pagination mt-5" aria-label="Pagination">
-            <a href="contact_us.php"><button type="button" class="btn btn-dark rounded-0">
+            <a href="contact_us"><button type="button" class="btn btn-dark rounded-0">
                 Got a project? Contact us!
               </button></a>
           </nav>
@@ -124,81 +105,60 @@ require_once("common_content/navbar.php");
       </div>
 
       <div class="col-md-4 shadow bg-white">
-        <div class="position-sticky" style="top: 2rem">
-          <div class="p-4 mb-3 bg-body-tertiary rounded">
-            <h3 class="fst-italic">About Our Service</h3>
-            <p class="mb-0">
-              Our graphic design services cover a wide range of products –
-              brochures, business cards, publications, mailers, posters and
-              billboards – we’ve pretty much designed it all. With nearly 6
-              years of experience designing for print and the web, we’ll help
-              your business convert potential clients into paying customers
-              with well designed, printed collateral.
-            </p>
-          </div>
+        <div class="position-sticky mt-5" style="top: 2rem">       
           <div>
             <h4 class="fst-italic">
               Our Recent project on Graphics Design
             </h4>
             <ul class="list-unstyled">
+                                    <?php
+                                       $sql4 = "SELECT * FROM portfolio WHERE category = 'Graphics Design'";
+                                       $stmt4 = $connectingDB->query($sql4);
+                                       while ($Data4 = $stmt4->fetch()) {
+                                           $portfolio_image = $Data4["image"];
+                                           $portfolio_name = $Data4["name"];
+                                       
+                                       ?>
               <li>
                 <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
-                  href="#">
-                  <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="#777"></rect>
-                  </svg>
+                  href="portfolio">
+                  <img class="bd-placeholder-img" width="100%" height="96" src="dashboard/images/<?php echo $portfolio_image; ?>" alt="dashboard/images/<?php echo $portfolio_image; ?>">
                   <div class="col-lg-8">
-                    <h6 class="mb-0">Example blog post title</h6>
-                    <small class="text-body-secondary">January 15, 2024</small>
+                    <h6 class="mb-0"><?php echo $portfolio_name; ?></h6>
                   </div>
                 </a>
               </li>
+              <?php } ?>
               <li>
-                <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
-                  href="#">
-                  <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="#777"></rect>
-                  </svg>
-                  <div class="col-lg-8">
-                    <h6 class="mb-0">This is another blog post title</h6>
-                    <small class="text-body-secondary">January 14, 2024</small>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
-                  href="#">
-                  <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="#777"></rect>
-                  </svg>
-                  <div class="col-lg-8">
-                    <h6 class="mb-0">
-                      Longer blog post title: This one has multiple lines!
-                    </h6>
-                    <small class="text-body-secondary">January 13, 2024</small>
-                  </div>
-                </a>
               </li>
             </ul>
           </div>
 
           <div class="p-4">
             <h4 class="fst-italic">Follow Us</h4>
+            <?php
+                                       $sql5 = "SELECT * FROM  contact_us";
+                                       $stmt5 = $connectingDB->query($sql5);
+                                       while ($Data5 = $stmt5->fetch()) {
+                                           $facebook = $Data5["fb"];
+                                           $instagram = $Data5["insta"];
+                                           $twitter = $Data5["tw"];
+                                           $whatsapp = $Data5["whatp"];
+
+                                       }
+                                       ?>
             <ol class="list-unstyled" style="display: flex">
               <li class="m-2 border bg-dark border-black shadow px-3 py-2 rounded-circle">
-                <a class="text-decoration-none text-white" href="#"><i class="fab fa-facebook"></i></a>
+                <a class="text-decoration-none text-white" href="<?php echo $facebook; ?>" target="_blank"><i class="fab fa-facebook"></i></a>
               </li>
               <li class="m-2 border bg-dark border-black shadow px-3 py-2 rounded-circle">
-                <a class="text-decoration-none text-white" href="#"><i class="fab fa-instagram"></i></a>
+                <a class="text-decoration-none text-white" href="<?php echo $instagram; ?>" target="_blank"><i class="fab fa-instagram"></i></a>
               </li>
               <li class="m-2 border bg-dark border-black shadow px-3 py-2 rounded-circle">
-                <a class="text-decoration-none text-white" href="#"><i class="fab fa-whatsapp"></i></a>
+                <a class="text-decoration-none text-white" href="<?php echo $whatsapp; ?>" target="_blank"><i class="fab fa-whatsapp"></i></a>
               </li>
               <li class="m-2 border bg-dark border-black shadow px-3 py-2 rounded-circle">
-                <a class="text-decoration-none text-white" href="#"><i class="fab fa-twitter"></i></a>
+                <a class="text-decoration-none text-white" href="<?php echo $twitter; ?>" target="_blank"><i class="fab fa-twitter"></i></a>
               </li>
             </ol>
           </div>
